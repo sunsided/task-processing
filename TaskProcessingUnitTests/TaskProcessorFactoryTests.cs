acadf4b9fe75b8c23dbf6c7d027c6bde50eaac1e
Markus Mayer <development@widemeadows.de>
2012-03-15 19:58:27 +0100

    added factory tests

---
+++ TaskProcessingUnitTests/TaskProcessorFactoryTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using TaskProcessing;
+
+namespace TaskProcessingUnitTests
+{
+    [TestFixture]
+    class TaskProcessorFactoryTests
+    {
+        private ITaskProcessorFactory<ITask> Factory;
+
+        [SetUp]
+        public void Setup()
+        {
+            Factory = new TaskProcessorFactory<ITask>();
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            Factory = null;
+        }
+
+        [Test]
+        public void Create_withNull_doesNotThrowAndReturnsInstance()
+        {
+            ITaskProcessor<ITask> processor = null;
+            Assert.That(() => { processor = Factory.Create(null); }, Throws.Nothing);
+            Assert.That(processor, Is.Not.Null);
+        }
+
+        [Test]
+        public void Create_withNoParameter_isIdenticalWithNullParameter()
+        {
+            Assert.That(Factory.Create().GetType(), Is.SameAs(Factory.Create(null).GetType()));
+        }
+    }
+}
