529aeb51d1f9613ff078617ebf3888a17592ead8
Markus Mayer <development@widemeadows.de>
2012-03-15 03:14:39 +0100

    made processor interface contravariant

--- TaskProcessing/ITaskProcessor.cs
+++ TaskProcessing/ITaskProcessor.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TaskProcessing
+﻿namespace TaskProcessing
 {
     /// <summary>
     /// Processor for tasks
     /// </summary>
     /// <typeparam name="TTask">The type of the task</typeparam>
-    public interface ITaskProcessor<TTask> where TTask : ITask
+    public interface ITaskProcessor<in TTask> where TTask : ITask
     {
         /// <summary>
         /// Adds a task
